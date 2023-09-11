2cb884eb32aad3ee07188faa4867b99a18a40867
Falcon <falcon.lin.development@gmail.com>
2023-09-11 13:16:14 +0800

    22-end

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'django_celery_beat', # scheduler
     "django_celery_results", # results
+    "django_htmx",
 
     # internal app
     "profiles",
@@ -68,7 +69,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
+    'allauth.account.middleware.AccountMiddleware',
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = 'cfehome.urls'
--- src/movies/views.py
+++ src/movies/views.py
@@ -1,20 +1,45 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import generic
 # Create your views here.
 
 from .models import Movie
 
+
+SORTING_CHOICES = {
+    "popular": "-rating_avg",
+    "unpopular": "rating_avg",
+    "recent": "-release_date",
+    "old": "release_date",
+}
+
 class MovieListView(generic.ListView):
     # model = Movie
-    template_name = 'movies/movie_list.html'
-    queryset = Movie.objects.all().order_by('-rating_avg')
+
     paginate_by = 100
+
+    def get_queryset(self) -> QuerySet[Any]:
+        request = self.request
+        default_sort = request.session.get('movie_sort_order', '-rating_avg')
+        qs = Movie.objects.all().order_by(default_sort)
+        sort = request.GET.get('sort')
+        if sort is not None:
+            request.session['movie_sort_order'] = sort
+            qs = qs.order_by(sort)
+        return qs
+
+    def get_template_names(self):
+        request = self.request 
+        if request.htmx:
+            return ['movies/snippet/list.html',]
+        return ['movies/movie_list.html', ]
 
     def get_context_data(self, **kwargs: Any):
         context = super().get_context_data(**kwargs)
         request = self.request
         user = request.user
+        context["sorting_choices"] = SORTING_CHOICES
         if user.is_authenticated:
             obj_ids = [x.id for x in context["object_list"]]
             context['my_ratings'] = user.rating_set.filter(active=True).as_object_dict(object_ids=obj_ids)
